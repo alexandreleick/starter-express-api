8bd0c7d0913690254a1599884d13ee7acf04e205
Alexandre Leick <alexandre.leick@epitech.eu>
2023-04-05 18:41:45 +0200

    init

--- old/index.js
+++ old/index.js
@@ -1,3 +1,4 @@
+
 const { createBullBoard } = require('@bull-board/api');
 const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
 const { ExpressAdapter } = require('@bull-board/express');
@@ -11,48 +12,49 @@ const LocalStrategy = require('passport-local').Strategy;
 const { ensureLoggedIn } = require('connect-ensure-login');
 
 passport.use(
-	new LocalStrategy(function (username, password, cb) {
-		if (username === 'bull' && password === 'board') {
-			return cb(null, { user: 'bull-board' });
-		}
-		return cb(null, false);
-	})
+  new LocalStrategy(function (username, password, cb) {
+    if (username === 'bull' && password === 'board') {
+      return cb(null, { user: 'bull-board' });
+    }
+    return cb(null, false);
+  })
 );
 
 passport.serializeUser((user, cb) => {
-	cb(null, user);
+  cb(null, user);
 });
 
 passport.deserializeUser((user, cb) => {
-	cb(null, user);
+  cb(null, user);
 });
 
 const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t * 1000));
 
 
 const redisOptions = {
-	port: 16823,
-	host: 'redis-16823.c253.us-central1-1.gce.cloud.redislabs.com',
-	password: 'muO3hLEXWLDFAPxEbgcq8QBcXfS1CD4e',
-	tls: false,
+  port: 16823,
+  host: 'redis-16823.c253.us-central1-1.gce.cloud.redislabs.com',
+  password: 'muO3hLEXWLDFAPxEbgcq8QBcXfS1CD4e',
+  tls: false,
 };
 
 const createQueueMQ = (name) => new QueueMQ(name, { connection: redisOptions });
 const emailQueue = createQueueMQ('Email');
 const pushQueue = createQueueMQ('Push Notification');
 const smsQueue = createQueueMQ('SMS');
-const app = express()
+
+const app = express();
 
 const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath('/ui');
 
 createBullBoard({
-	queues: [
-		new BullMQAdapter(emailQueue),
-		new BullMQAdapter(pushQueue),
-		new BullMQAdapter(smsQueue),
-	],
-	serverAdapter,
+  queues: [
+    new BullMQAdapter(emailQueue),
+    new BullMQAdapter(pushQueue),
+    new BullMQAdapter(smsQueue),
+  ],
+  serverAdapter,
 });
 
 app.set('views', __dirname + '/views');
@@ -66,27 +68,28 @@ app.use(passport.initialize({}));
 app.use(passport.session({}));
 
 app.get('/ui/login', (req, res) => {
-	res.render('login', { invalid: req.query.invalid === 'true' });
+  res.render('login', { invalid: req.query.invalid === 'true' });
 });
 
 app.post(
-	'/ui/login',
-	passport.authenticate('local', { failureRedirect: '/ui/login?invalid=true' }),
-	(req, res) => {
-		res.redirect('/ui');
-	}
+  '/ui/login',
+  passport.authenticate('local', { failureRedirect: '/ui/login?invalid=true' }),
+  (req, res) => {
+    res.redirect('/ui');
+  }
 );
 
 
 app.use('/ui', ensureLoggedIn({ redirectTo: '/ui/login' }), serverAdapter.getRouter());
 
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 3000
 
 app.listen(port, (err, res) => {
-	if (err) {
-		console.log(err)
-		return res.status(500).send(err.message)
-	} else {
-		console.log('[INFO] Server Running on port:', port)
-	}
+    if (err) {
+        console.log(err)
+        return res.status(500).send(err.message)
+    } else {
+        console.log('[INFO] Server Running on port:', port)
+    }
 })
+
